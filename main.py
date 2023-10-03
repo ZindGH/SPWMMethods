6394470b59cf752cc3b464923f37cedea61b07dc
ZindHP <zzzRRzzz@mail.ru>
2023-10-03 10:33:15 +0300

    One method before test

--- main.py
+++ main.py
@@ -9,96 +9,117 @@ import plotly.graph_objects as go
 import scipy
 from scipy import signal
 
-def makeSine(n_samples: int = 10000, shift_tri: int = 25, F_tri: int = 2800, eps: float = 0.02, sin_shift: int = 100,
-             method: str = "PosCarrier PosSine SPWM"):
-    amp = 3
+print(1)
+
+
+def makeSineReworked(amp: float = 0.90, pwm_T: int = 10000, F_tri: int = 2800, sin_shift: int = 0,
+                     number_of_T: float = 0.5, method: str = "PCPS", tri_sign=True, pwm_inv=0):
     F_sin = 400
-    # F_tri = 3000
-    tan = 15 * np.power(10, 3)
-
-    Tsin_half = 1 / 2 * (1 / F_sin)
-
-    t = np.linspace(0, Tsin_half, n_samples)
-    Td = t[1] - t[0]
-    mflag = None  # Method abbr
+    Tsin = (1 / F_sin)
     match method:
-        case ("PosCarrier PosSine SPWM"):
-            triangle = 4 / 2 * (signal.sawtooth(2 * np.pi * F_tri * (t - sin_shift * Td), 0.5) + 1)
-            triangle = np.concatenate(([0] * shift_tri, triangle))
-            triangle = np.roll(triangle, shift_tri)[shift_tri:]
+        case "PCPS":
+            n_samples = int(number_of_T * pwm_T * F_tri / 400)
+        case "PNCDS":
+            n_samples = int(number_of_T * pwm_T * F_tri / 400)
+
+    t = np.linspace(0, Tsin * number_of_T, n_samples)
+    Td = t[1] - t[0]
+
+    match method:
+        case ("PCPS"):
+            sinus = amp * np.sin(2 * np.pi * F_sin * (t - sin_shift / 360))
+            if tri_sign:
+                triangle = 1 / 2 * (signal.sawtooth(2 * np.pi * F_tri * t, 0.5) + 1)
+                sinus = np.abs(sinus)
+            else:
+                triangle = signal.sawtooth(2 * np.pi * F_tri * t, 0.5)
 
             plt.plot(t / Td, triangle)  #
-
-            sinus = amp * np.sin(2 * np.pi * F_sin * t)
 
             plt.plot(t / Td, sinus)
 
-            mflag = "PCPS"
-        case ("PNCarrier DualSim SPWM"):
+        case ("PNCDS"):
+            triangle = 1 * (signal.sawtooth(2 * np.pi * F_tri * t, 0.5))
+            plt.plot(t / Td, triangle)
 
-            triangle = 6 / 2 * (signal.sawtooth(2 * np.pi * F_tri * (t - sin_shift * Td), 0.5))
-            triangle = np.concatenate(([0] * shift_tri, triangle))
-            triangle = np.roll(triangle, shift_tri)[shift_tri:]
-
-            plt.plot(t / Td, triangle)  #
-
-            sinus = amp * np.sin(2 * np.pi * F_sin * t)
-            sinusN = -amp * np.sin(2 * np.pi * F_sin * t)
+            sinus = amp * np.sin(2 * np.pi * F_sin * (t - sin_shift / 360))
+            sinusN = -amp * np.sin(2 * np.pi * F_sin * (t - sin_shift / 360))
 
             plt.plot(t / Td, sinus)
             plt.plot(t / Td, sinusN)
-            mflag = "PNCDS"
 
         case _:
             raise f"tvoi method: {method} --- NOT IMPLEMENTED"
 
-    # match phase_sin:
-    #     case '0':
-    #         t_sin = t
-    #         t_tri = np.pad(t, (shift_tri, 0), mode='constant')
-    #     case '120':
-    #         t_sin = np.pad(t, ((1 / F_sin) / 3 - Td, 0), mode='constant')
-    #         t_tri = np.pad(t, ((1 / F_sin) / 3 - Td + shift_tri, 0), mode='constant')
-    #     case '240':
-    #         t_sin = np.pad(t, (2 * (1 / F_sin) / 3 - Td, 0), mode='constant')
-    #         t_tri = np.pad(t, (2 * (1 / F_sin) / 3 - Td + shift_tri, 0), mode='constant')
-
     res_t = list()
-    just_got_value = 0
-    for i in range(len(t)):
-        if i <= shift_tri:
-            continue
-        if 0 < just_got_value < n_samples / (1000 * 1.5):
-            just_got_value += 1
-            continue
-        elif just_got_value >= n_samples / (1000 * 1.5) or just_got_value == 0:
-            just_got_value = 0
-        if mflag == "PNCDS":
-            if (abs(triangle[i] - sinus[i])) <= eps or (abs(triangle[i] - sinusN[i])) <= eps:
-                res_t.append((triangle[i], i))
-                just_got_value += 1
-        elif mflag == "PCPS":
-            if (abs(triangle[i] - sinus[i])) <= eps:
-                res_t.append((triangle[i], i))
-                just_got_value += 1
+    sign_PCPS: int = 1
+    sign_PNCDS_N = 1  #
+    sign_PNCDS = 1
+    # First check:
+    match method:
+        case "PCPS":
+            if ((sinus[1] - triangle[1]) * sign_PCPS) > 0:
+                sign_PCPS *= -1
+        case "PNCDS":
+            if ((sinus[1] - triangle[1]) * sign_PNCDS) > 0:
+                sign_PNCDS = 1
+            if ((sinusN[1] - triangle[1]) * sign_PNCDS_N) > 0:
+                sign_PNCDS_N = -1
+
+    for i in range(2, len(t) - 1):
+        match method:
+            case "PCPS":
+                if ((sinus[i] - triangle[i]) * sign_PCPS) > 0:
+                    res_t.append(i)
+                    sign_PCPS *= -1
+            case "PNCDS":
+                if ((sinus[i] - triangle[i]) * sign_PNCDS) > 0:
+                    res_t.append(i)
+                    sign_PNCDS *= -1
+                if ((sinusN[i] - triangle[i]) * sign_PNCDS_N) > 0:
+                    res_t.append(i)
+                    sign_PNCDS_N *= -1
 
     print(f"array: {res_t}\narray_len: {len(res_t)}")
-    # print(min_debug_val)
-    pwm_temp_val = [i for _, i in res_t][0:]
+
+    pwm_temp_val = [i for i in res_t][0:]
+    pair_centers = list()
+    pair_diffs = list()
+    new_pwm = list()
+    if pwm_inv:
+        subList = [pwm_temp_val[n:n + 2] for n in range(0, len(pwm_temp_val), 2)]
+        for k, pair in enumerate(subList):
+            center = round((pair[0] + pair[1]) / 2)
+            diff = pwm_T - abs(pair[1] - pair[0])
+            k=0
+            new_pwm.append(round((center - diff / 2) + (k * pwm_T)))
+            new_pwm.append(round((center + diff / 2) + (k * pwm_T)))
+            pwm_temp_val = new_pwm
+
     res_pwm = list()
     flag_on = False
+    pwm_amp = -0.3
     for i in range(n_samples):
         if i in pwm_temp_val:
             flag_on = not flag_on
-        if flag_on:
-            res_pwm.append(1)
-        else:
-            res_pwm.append(0)
+        match method:
+            case "PCPS":
+                if flag_on:
+                    pwm_amp = 0.5
+                    res_pwm.append(pwm_amp)
+                else:
+                    res_pwm.append(0)
+            case "PNCDS":
+                if flag_on:
+                    res_pwm.append(pwm_amp)
+                else:
+                    res_pwm.append(-pwm_amp)
 
     res_pwm = np.array(res_pwm)
+
     plt.plot(t / Td, res_pwm)
 
-    return [round(x, 4) for x, _ in res_t], [i for _, i in res_t], Td
+    return res_t, Td
 
 
 def print_c_array(array, wideness: int = 8):
@@ -115,17 +136,13 @@ def print_c_array(array, wideness: int = 8):
     print("\n}")
 
 
-
-
-
-
 if __name__ == '__main__':
-    T_samples = 10000
-    _, time_array, Td = makeSine(n_samples=14 * T_samples, F_tri=2800 * 2, shift_tri=0, eps=0.001,
-                                 sin_shift=T_samples)
+    T_samples = 16667
+    time_array, Td = makeSineReworked(amp=0.89, pwm_T=10000, F_tri=5600, sin_shift=120,
+                                      number_of_T=1, method="PCPS", tri_sign=1, pwm_inv=True)
 
-    # _, time_array, Td = makeSine(n_samples=T_samples * 7, F_tri=2800, shift_tri=0, eps=0.003, sin_shift=0,
-    #                              method="PNCarrier DualSim SPWM")
+    # time_array, Td = makeSineReworked(amp=0.90, pwm_T=10000, F_tri=2800, sin_shift=100,
+    #                                     number_of_T=0.5, method="PCPS")
 
     time_array = [spwm_t % T_samples for spwm_t in time_array]
     ds = T_samples - time_array[-1] + time_array[0]
@@ -134,4 +151,3 @@ if __name__ == '__main__':
 
     print_c_array(time_array, wideness=4)
     plt.show()
-
